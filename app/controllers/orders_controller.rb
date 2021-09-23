0633f1084348dd1350c3c9482accbb9496303326
hienbt <buithehien1991@gmail.com>
2021-09-23 14:55:31 +0700

    Can select customer with order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -64,6 +64,6 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:quantity)
+      params.require(:order).permit(:quantity, :customer_id)
     end
 end
